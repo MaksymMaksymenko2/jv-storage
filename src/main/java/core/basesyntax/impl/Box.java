4c926d299c935a8f7cb945e1c0a83d259536e4f9
Maksym Maksymenko <Maksimfonmm2@gmail.com>
2025-07-21 18:43:16 +0300

    пис коміту: додано новий функціонал

---
+++ src/main/java/core/basesyntax/impl/Box.java
@@ -0,0 +1,10 @@
+package core.basesyntax.impl;
+
+public class Box {
+    private String content = "something";
+
+    @Override
+    public String toString() {
+        return "Box{" + "content='" + content + '\'' + '}';
+    }
+}
--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,53 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+
+    private static final int MAX_STORAGE_SIZE = 10;
+
+    private final K[] keys;
+    private final V[] values;
+    private int currentSize;
+
+    @SuppressWarnings("unchecked")
+    public StorageImpl() {
+        keys = (K[]) new Object[MAX_STORAGE_SIZE];
+        values = (V[]) new Object[MAX_STORAGE_SIZE];
+        currentSize = 0;
+    }
+
     @Override
     public void put(K key, V value) {
+        int index = findIndexByKey(key);
+
+        if (index != -1) {
+            values[index] = value;
+        } else if (currentSize < MAX_STORAGE_SIZE) {
+            keys[currentSize] = key;
+            values[currentSize] = value;
+            currentSize++;
+        }
     }
 
     @Override
     public V get(K key) {
+        int index = findIndexByKey(key);
+        if (index != -1) {
+            return values[index];
+        }
         return null;
     }
 
     @Override
     public int size() {
+        return currentSize;
+    }
+
+    private int findIndexByKey(K key) {
+        for (int i = 0; i < currentSize; i++) {
+            if ((key == null && keys[i] == null) || (key != null && key.equals(keys[i]))) {
+                return i;
+            }
+        }
         return -1;
     }
 }
